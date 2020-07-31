858afb33288d742c1d4bfa4e829aee0366f579f5
Nhat Ho <honhattan2411@gmail.com>
2020-07-30 17:59:37 -0700

    Fix url

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-
+const api = {
+  key: 'd0d8783997386500229ba9ab6c7e7592',
+  base: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/'
+}
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = e => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        //console.log(weather.main.temp);
+      })
+      .catch(err =>{
+        return err;
+      });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className={(weather.main && weather.main.temp > 16) ? 'app warm': 'app'}>
+      <main>
+        <div className='search-box'>
+          <input
+            type='text'
+            className='search-bar'
+            placeholder='Search...'
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+
+        {weather.main ? 
+        (
+          <div>
+            <div className='location-box'>
+              <div className='location'>{weather.name}, {weather.sys.country}</div>
+              <div className='date'>{new Date().toDateString()}</div>
+            </div>
+
+            <div className='weather-box'>
+              <div className='temp'>{Math.round(weather.main.temp)}*C</div>
+              <div className='weather'>{weather.weather[0].main}</div>
+            </div>
+          </div>
+        )
+        : null
+        }
+      </main>
     </div>
   );
 }
