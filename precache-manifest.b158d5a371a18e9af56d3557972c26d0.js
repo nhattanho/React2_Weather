16519c682599f76ec46b2d106ff2886a276d76b4
Nhat Ho <honhattan2411@gmail.com>
2020-07-30 17:51:37 -0700

    Updates

---
+++ precache-manifest.b158d5a371a18e9af56d3557972c26d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e97dfb0ca762007333334ce118a56cc",
+    "url": "/React2_Weather/index.html"
+  },
+  {
+    "revision": "64082dafeb5bbd96d6e3",
+    "url": "/React2_Weather/static/css/main.56d90b45.chunk.css"
+  },
+  {
+    "revision": "43f581cd7f8eb4790ba1",
+    "url": "/React2_Weather/static/js/2.1cf1a1f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React2_Weather/static/js/2.1cf1a1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64082dafeb5bbd96d6e3",
+    "url": "/React2_Weather/static/js/main.f9048269.chunk.js"
+  },
+  {
+    "revision": "9f2ae0e157669dd7c001",
+    "url": "/React2_Weather/static/js/runtime-main.645a0ee6.js"
+  },
+  {
+    "revision": "26caeab93ac44fdb8a8d4d03d42e85ca",
+    "url": "/React2_Weather/static/media/cold.26caeab9.jpg"
+  },
+  {
+    "revision": "9c1418286876a82cbe9d57deb75c645d",
+    "url": "/React2_Weather/static/media/warm.9c141828.jpg"
+  }
+]);
