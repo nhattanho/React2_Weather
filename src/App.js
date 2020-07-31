0a18f298a87312082c0b3602ec562789be032ee6
Nhat Ho <honhattan2411@gmail.com>
2020-07-30 19:48:33 -0700

    Adding heading tag

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 const api = {
   key: 'd0d8783997386500229ba9ab6c7e7592',
-  base: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/'
+  base: 'https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/'
 }
 function App() {
   const [query, setQuery] = useState('');
@@ -27,17 +27,17 @@ function App() {
     
     <div className={(weather.main && weather.main.temp > 16) ? 'app warm': 'app'}>
       <main>
+        <div className='heading'><h1> Welcome to Weather Tracker</h1></div>
         <div className='search-box'>
           <input
             type='text'
             className='search-bar'
-            placeholder='Search...'
+            placeholder='Search your location...'
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
-
         {weather.main ? 
         (
           <div>
